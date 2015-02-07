8aece54af184fec7e49670d7c97689b8c233de3f
Jason Britton <velograph@gmail.com>
2015-02-07 09:16:01 -0800

    Remove Typekit.

--- functions.php
+++ functions.php
@@ -210,20 +210,3 @@ function what_can_i_do() {
 
 // Hook into the 'init' action
 add_action( 'init', 'what_can_i_do', 0 );
-
-/**
-* TypeKit Fonts
-*
-* @since Theme 1.0
-*/
-function theme_typekit() {
-	wp_enqueue_script( 'theme_typekit', '//use.typekit.net/ncp1eax.js');
-}
-add_action( 'wp_enqueue_scripts', 'theme_typekit' );
-
-function theme_typekit_inline() {
-if ( wp_script_is( 'theme_typekit', 'done' ) ) { ?>
-	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-<?php }
-}
-add_action( 'wp_head', 'theme_typekit_inline' );
