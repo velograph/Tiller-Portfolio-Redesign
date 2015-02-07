64145eac4b2b8366c64c528c681f71820d1a3a53
Jason Britton <velograph@gmail.com>
2015-02-07 11:04:01 -0800

    Case Study list styling and mini-lead-in-thingy.

--- index.php
+++ index.php
@@ -24,19 +24,19 @@ get_header(); ?>
 				<?php $the_content = apply_filters('the_content', get_post(25)->post_content); ?>
 				<?php echo $the_content; ?>
 
+				<h3><a href="/contact">Let's Chat!</a></h3>
+
+				<h4><a href="#">See Work</a></h4>
+
+				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+					width="30px" height="13.084px" viewBox="0 0 30 13.084" enable-background="new 0 0 30 13.084" xml:space="preserve">
+				<polygon fill="#C2C8CA" points="28.004,2.123 15.056,10.698 1.716,2.123 0,2.123 0,2.229 14.275,11.337 15.835,11.337 29.721,2.229
+					29.721,2.123 "/>
+				</svg>
+
 			</div>
 
 		</article>
-
-		<h3><a href="/contact">Let's Chat!</a></h3>
-
-		<h4><a href="#">See Work</a></h4>
-
-		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-			width="30px" height="13.084px" viewBox="0 0 30 13.084" enable-background="new 0 0 30 13.084" xml:space="preserve">
-		<polygon fill="#C2C8CA" points="28.004,2.123 15.056,10.698 1.716,2.123 0,2.123 0,2.229 14.275,11.337 15.835,11.337 29.721,2.229
-			29.721,2.123 "/>
-		</svg>
 
 		<?php if ( have_posts() ) : ?>
 
@@ -47,18 +47,17 @@ get_header(); ?>
 					<div class="case-study">
 
 						<a href="<?php the_permalink(); ?>">
-							<div class="case-study-title">
-								<span>&mdash;&nbsp;Case Study&nbsp;&mdash;</span>
-								<h2><?php the_title(); ?></h2>
-							</div>
-
-							<div class="box outside-border"></div>
-
-							<div class="box inside-border"></div>
 
 							<div class="featured-image-container">
+
+								<div class="case-study-title">
+									<span>&mdash;&nbsp;Case Study&nbsp;&mdash;</span>
+									<h2><?php the_title(); ?></h2>
+								</div>
+
 								<div class="hover-border" style="border-color:<?php the_field('border_color'); ?>"></div>
 								<?php the_post_thumbnail(); ?>
+
 							</div>
 						</a>
 
